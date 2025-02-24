1f4fb65c8245786a9150d70e2bfc88fdc35d2432
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-02-24 10:51:53 +0530

    Created_component

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,11 @@ let a= false;
 
 
 mongoose.connect(process.env.DB_URL)
-.then(()=>{console.log("Connected to database")
+.then(()=>{
+    console.log("Connected to database")
     a = true;
 })
-.catch(()=>{console.log("Error connecting to database")})
+.catch((err)=>console.log("Error connecting to database"+err))
 
 app.get("/",(req, res)=>{
     if(a){
