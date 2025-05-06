0c488ee4997f33ee41ec63d5065b0e9d1179b179
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-05-06 20:10:47 +0530

    Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,3 +1,16 @@
+// const mongoose = require("mongoose");
+
+// const messageSchema = new mongoose.Schema({
+//   senderId: { type: String, required: true },
+//   text: { type: String, required: true },
+//   recipientId: { type: String },
+//   isPrivate: { type: Boolean, default: false },
+//   timestamp: { type: Date, default: Date.now },
+//   replyTo: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+//   seenAt: { type: Date },
+// });
+
+// module.exports = mongoose.model("Message", messageSchema);
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
@@ -8,6 +21,12 @@ const messageSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now },
   replyTo: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
   seenAt: { type: Date },
+  reactions: [
+    {
+      emoji: { type: String, required: true },
+      userId: { type: String, required: true },
+    },
+  ],
 });
 
 module.exports = mongoose.model("Message", messageSchema);
